9d3e1b91dda8b36f14d884ae9434f8b84b620b2f
AlexZStefan <72689002+AlexZStefan@users.noreply.github.com>
2025-03-01 17:36:01 +0000

    Enemies will spawn depending on player position

--- public/EnemySpawner.js
+++ public/EnemySpawner.js
@@ -7,9 +7,15 @@ export default class EnemySpawner {
         this.time = 0;
     }
 
+    // create enemy based on the player y position
     CreateEnemy() {
-        let y = Math.floor(Math.random() * 100);
-        console.log(y);
+        let playerPosition = this.scene.player.y;
+        // top
+        let y = 170;
+        // middle 
+        if(playerPosition < 400 && playerPosition >211 ) y = 359;
+        // bottom
+        else if(playerPosition < 600 && playerPosition >400 ) y = 500;
         let x = this.scene.sys.game.canvas.width;
         this.enemies.push(new Enemy(this.scene, x, y, "dude"))
     }
