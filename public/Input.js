5473220b43e143386c8ae04819e2b513dcdecdea
AlexZStefan <72689002+AlexZStefan@users.noreply.github.com>
2025-03-01 17:36:36 +0000

    Disabled Player movement on death

--- public/Input.js
+++ public/Input.js
@@ -4,33 +4,37 @@
 export default class InputHandler {
     constructor(scene, player) {
         this.scene = scene;
-        this.player = player;
         // Set up the cursors for input
         this.cursors = this.scene.input.keyboard.createCursorKeys();
+
     }
 
-    /**
-     * Update the input handling.
-     */
+    characterMovement() {
+        let player = this.scene.player;
+        if(!player.dead)
+        {
+            if (this.cursors.left.isDown) {
+                player.setVelocityX(-160);
+                
+                player.anims.play('left', true);
+            }
+            else if (this.cursors.right.isDown) {
+                player.setVelocityX(160);
+                
+                player.anims.play('right', true);
+            }
+            else {
+                player.setVelocityX(0);
+                
+                player.anims.play('turn');
+            }
+            if (this.cursors.up.isDown && player.body.touching.down) {
+                player.setVelocityY(-330);
+            }
+        }
+    }
+
     update() {
-        if (this.cursors.left.isDown) {
-            this.player.setVelocityX(-160);
-    
-            this.player.anims.play('left', true);
-        }
-        else if (this.cursors.right.isDown) {
-            this.player.setVelocityX(160);
-    
-            this.player.anims.play('right', true);
-        }
-        else {
-            this.player.setVelocityX(0);
-    
-            this.player.anims.play('turn');
-        }
-    
-        if (this.cursors.up.isDown && this.player.body.touching.down) {
-            this.player.setVelocityY(-330);
-        }
+        this.characterMovement();
     }
 }
