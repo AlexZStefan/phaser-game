e444d5644917ef2c25c523bb741d75f70fa0fc0d
AlexZStefan <72689002+AlexZStefan@users.noreply.github.com>
2025-03-01 17:38:51 +0000

    Display GameOver scene upon death

--- public/Player.js
+++ public/Player.js
@@ -1,20 +1,26 @@
 import Inventory from "./Inventory.js";
 import Humanoid from "./Humanoid.js";
+import GameScene from "./Game.js";
 
 export default class Player extends Humanoid {
     /**
-     * @param {Phaser.Scene} scene 
+     * @param {GameScene} scene 
      * @param {number} x 
      * @param {number} y 
      * @param {string} texture 
      */
     constructor(scene, x, y, texture) {
         super(scene, x, y, texture);
-
         // Add an inventory system (if applicable)
-        this.health = 100;
         this.inventory = new Inventory(scene);
-    
-        // No need for `return this;`
+        this.scene = scene;
+    }
+
+    triggerDeath(){
+        this.setTint(0xff0000);
+        this.anims.play('turn');
+        this.scene.physics.pause();
+        this.dead = true;
+        this.scene.scene.launch("GameOver"); // Open pause menu
     }
 }
