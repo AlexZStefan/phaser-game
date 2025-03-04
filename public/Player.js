f505f2b15e61151e5ffa694c098cce2c25e3ee37
AlexZStefan <72689002+AlexZStefan@users.noreply.github.com>
2025-03-04 20:15:18 +0000

    Added attack/slash with animation

--- public/Player.js
+++ public/Player.js
@@ -11,39 +11,48 @@ export default class Player extends Humanoid {
      */
     constructor(scene, x, y, texture) {
         super(scene, x, y, texture);
-        // Add an inventory system (if applicable)
         this.inventory = new Inventory(scene);
         this.scene = scene;
         this.attackSpeed = 5;
-        this.canAttack = true;    
+        this.canAttack = true;
         this.timeBetweenAttack = 0.5;
-        this.isAttacking = false;
 
-        const assets= this.scene.cache.json.get('playerSprites');     
-        
         this.on('animationcomplete', (animation) => {
             if (animation.key === 'attack') {
-                console.log("aaa");
-                this.isAttacking = false; // Reset attack flag
-                this.anims.play('turn', true); // Go back to idle/turn animation
+                this.isAttacking = false;
+                this.anims.play('turn', true);
             }
         });
     }
-   
+
     attack() {
+        this.anims.play('attack', true);
+        this.isAttacking = true;
         let forwardX = this.body.velocity.x;
-        if(forwardX != 0 && this.canAttack){
+
+        if (forwardX != 0 && this.canAttack) {
             this.canAttack = false;
-            let bomb = this.scene.physics.add.sprite(this.x, this.y, "bomb");
-            bomb.body.setAllowGravity(false); 
+
+            // Create individual projectile
+            let bomb = this.scene.physics.add.sprite(this.x, this.y, "slash");
+            bomb.setScale(3);
+            bomb.anims.create({
+                key: 'slash',
+                frames: this.anims.generateFrameNumbers('slash', { start: 0, end: 3 }),
+                frameRate: 5,
+                repeat: 0
+            });
+            bomb.anims.play('slash', true);
+            bomb.body.setAllowGravity(false);
             bomb.setVelocityX(forwardX * this.attackSpeed);
             // should replace this with groups
             this.scene.enemySpawner.enemies.forEach(enemy => {
-                this.scene.physics.add.overlap(bomb, enemy, ()=> {
-                    enemy.triggerDeath(); 
+                this.scene.physics.add.overlap(bomb, enemy, () => {
+                    enemy.triggerDeath();
                     bomb.destroy();
                 }, null, this);
             });
+
             // should have a pool of objects here 
             this.scene.time.delayedCall(this.timeBetweenAttack * 1000, () => {
                 bomb.destroy();
@@ -54,12 +63,10 @@ export default class Player extends Humanoid {
 
     triggerDeath() {
         this.setTint(0xff0000);
-        
         this.scene.physics.pause();
         this.dead = true;
         this.anims.play('dead', true);
-
-        this.scene.time.delayedCall( 1000, () => {
+        this.scene.time.delayedCall(1000, () => {
             this.scene.scene.launch("GameOver"); // Open pause menu
         });
     }
