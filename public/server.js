478f82566f0bbfa3beccf28aea162de855108892
AlexZStefan <72689002+AlexZStefan@users.noreply.github.com>
2025-03-04 15:07:50 +0000

    misc

--- public/server.js
+++ public/server.js
@@ -1,8 +1,11 @@
-const express = require('express');
-const app = express();
+import express from 'express';
+import path from 'path';
 
-app.use(express.static('public'));
+const app = express();
+const __dirname = path.resolve(); // To resolve __dirname in ES Modules
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(3000, () => {
-    console.log('Server running at http://localhost:3000');
+  console.log('Server running on http://localhost:3000');
 });
